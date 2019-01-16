06929ae02dc10a5e47bd657d073eb59fa0283a5f
Dan Klco <klcodanr@users.noreply.github.com>
2019-01-16 18:28:21 -0500

    Adding support for creating / deleting groups via repoinit (#2)
    Fixing as per SLING-8219

--- src/main/java/org/apache/sling/repoinit/parser/operations/package-info.java
+++ src/main/java/org/apache/sling/repoinit/parser/operations/package-info.java
@@ -15,6 +15,6 @@
  * limitations under the License.
  ******************************************************************************/
 
-@org.osgi.annotation.versioning.Version("4.1.0")
+@org.osgi.annotation.versioning.Version("4.2.0")
 package org.apache.sling.repoinit.parser.operations;
 
